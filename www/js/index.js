b9ba54cd56bf2460b1aca27fb8c14bf6934e0ef3
jasj <jasj@DESKTOP-P253GI3.localdomain>
2020-07-14 00:03:26 -0600

    first

--- www/js/index.js
+++ www/js/index.js
@@ -120,8 +120,6 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        initPushwoosh();
-
         getAllSMS();
         app.receivedEvent('deviceready');
 
